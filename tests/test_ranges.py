014a94daf8effc74ab37b48f8cf30a52c3644149
Fedor P. Goncharov <gfederix@gmail.com>
2018-11-22 18:10:43 +0700

    test_long_range_with_data_show

--- tests/test_ranges.py
+++ tests/test_ranges.py
@@ -59,4 +59,24 @@ def test_long_range_with_data_show(test_out, capsys):
         11         1 |      11.0      0.11
         12         1 |      12.0      0.12
 ''')
+    Ranges(start=range(10), width=1, data = {
+        'score' : np.array(range(0, 10), dtype="f4"),
+        'fscore' : np.array(range(0, 10), dtype="f4") /100,
+    }).show()
+    x = capsys.readouterr().out
+    assert x == (
+'''Range with 10 ranges:
+     start     width |     score    fscore
+  <uint32>  <uint32> | <float32> <float32>
+         0         1 |       0.0       0.0
+         1         1 |       1.0      0.01
+         2         1 |       2.0      0.02
+         3         1 |       3.0      0.03
+         4         1 |       4.0      0.04
+         5         1 |       5.0      0.05
+         6         1 |       6.0      0.06
+         7         1 |       7.0      0.07
+         8         1 |       8.0      0.08
+         9         1 |       9.0      0.09
+''')
 
