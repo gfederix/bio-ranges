e1cd7c55399ea7abb98de1d9354ef4a789250555
Fedor P. Goncharov <gfederix@gmail.com>
2018-12-19 04:58:02 +0700

    find_span mockup

--- bioRanges/nclist.pyx
+++ bioRanges/nclist.pyx
@@ -1,8 +1,11 @@
+#cython: language_level=3
 # from cpython cimport array
 # import array
 import numpy as np
+cimport numpy as np
 cimport cython
 from libc.stdlib cimport qsort
+from cython cimport view
 
 cdef int* start_p
 cdef int* end_p
@@ -20,6 +23,7 @@ cdef int compar_by_satart_width(const void* a, const void* b) nogil:
 cpdef int[::1] order_by_start_width(int[::1] index, int[::1] start, int[::1] end) nogil:
     """
     return order of array first sorted by start then sorted by width
+    np.ndarray[np.int_t,ndim=1]
     """
     global start_p, end_p
     cdef size_t size = start.shape[0]
@@ -30,3 +34,11 @@ cpdef int[::1] order_by_start_width(int[::1] index, int[::1] start, int[::1] end
     end_p = &end[0]
     qsort(index_p, size, sizeof(int), compar_by_satart_width)
     return index
+
+cpdef int find_span(int[::1] index,  int i_start, int i_end,
+                    int[::1] start, int[::1] end) nogil:
+    cdef int width = 0
+    cdef int idx = 0
+    for i in range(i_start, i_end):
+        idx = index[i]
+    return width
