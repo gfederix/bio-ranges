69f208328f6839c4b3027a3d11e7507434f41ef3
Fedor P. Goncharov <gfederix@gmail.com>
2018-12-19 22:23:26 +0700

    new NCList struct

--- bioRanges/nclist.pyx
+++ bioRanges/nclist.pyx
@@ -6,34 +6,52 @@ cimport numpy as np
 cimport cython
 from libc.stdlib cimport qsort
 from cython cimport view
+from libc.stdlib cimport malloc, free
+from libc.stdio cimport printf
 
 cdef int* start_p
 cdef int* end_p
+ctypedef struct NLshift:
+    int shift
+    int len
+
+ctypedef struct NLidx:
+    int idx
+    NLshift* ptr
+
 cdef int compar_by_satart_width(const void* a, const void* b) nogil:
     global start_p, end_p
-    ai = (<int*>a)[0]
-    bi = (<int*>b)[0]
+    ai = (<NLidx*>a).idx
+    bi = (<NLidx*>b).idx
     cdef int ret = start_p[ai] - start_p[bi]
-    if ret !=0:
+    if ret != 0:
         return ret
     return end_p[bi] - end_p[ai]
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef int[::1] order_by_start_width(int[::1] index, int[::1] start, int[::1] end) nogil:
+cdef void  _order_by_start_width(int[::1] start, int[::1] end, NLidx* idx_p) nogil:
     """
     return order of array first sorted by start then sorted by width
     np.ndarray[np.int_t,ndim=1]
     """
     global start_p, end_p
     cdef size_t size = start.shape[0]
-    cdef int* index_p = &index[0]
     for i in range(size):
-        index_p[i] = i
+        idx_p[i].idx = i
+        idx_p[i].ptr = NULL
     start_p = &start[0]
     end_p = &end[0]
-    qsort(index_p, size, sizeof(int), compar_by_satart_width)
-    return index
+    qsort(idx_p, size, sizeof(NLidx), compar_by_satart_width)
+
+def order_by_start_width(int[::1] start, int[::1] end):
+    cdef size_t size = start.shape[0]
+    cdef NLidx *idx = <NLidx*> malloc(sizeof(NLidx) * size)
+    _order_by_start_width(start, end, idx)
+    return np.array([idx[i].idx for i in range(size)], dtype=np.intc)
+
+cpdef void index(int[::1] start, int[::1] end, int[::1] idx) nogil:
+    pass
 
 cpdef int find_span(int[::1] index,  int i_start, int i_end,
                     int[::1] start, int[::1] end) nogil:
--- tests/test_nclist.py
+++ tests/test_nclist.py
@@ -2,16 +2,41 @@ import pytest
 import numpy as np
 # import pyximport
 # pyximport.install()
-from bioRanges.nclist import order_by_start_width
+from bioRanges.nclist import order_by_start_width, index
 
 def test_nclist_sort():
-    #                 0  1   2  3  4
+    #                 0  1  2   3  4
     start = np.array([1, 5,  5, 6, 3], dtype="i")
-    end = np.array([2, 3, 10, 8, 4], dtype="i")
-    index = np.array(range(5), dtype=np.intc)
-    o = order_by_start_width(index, start, end)
-    print(o)
-    for i in o:
-        print(i)
-    assert all(o == np.array([0, 4, 2, 1, 3]))
+    end   = np.array([2, 7, 10, 9, 5], dtype="i")
+    # Raw index
+    # I 123456789012345
+    # 0 --
+    # 1     ---
+    # 2     -------
+    # 3      ---
+    # 4    --
+    idx = order_by_start_width(start, end)
+    assert all(idx == np.array([0, 4, 2, 1, 3]))
+    # Sorted index
+    # I 123456789012345
+    # 0 --        
+    # 4   --      
+    # 2     -------
+    # 1     ---
+    # 3      ---
 
+    index(start, end, idx)
+    # NCList index
+    #   I 12345678901
+    # 0 1     ---    
+    # 1 3      ---   
+    # 2 0 --         
+    # 3 4   --       
+    # 4 2     -------
+    #
+    # N L
+    # 0 2 {}
+    # 2 3 {2:0, }
+    #
+    # (2,3, {2: (0,2, {})}
+    pass
